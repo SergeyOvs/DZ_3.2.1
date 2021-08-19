3cdaad02d3763aa6ea59defb8276977eba221454
Sergey Ovsyannikov <89055616288@mail.ru>
2021-08-19 20:04:10 +0300

    Добавьте файлы проекта.

---
+++ DZ_3.2.1/DZ_3.2.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void Swap( int& a,  int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int x1 = 10;
+    int x2 = 20;
+
+    cout << "X1="<< x1 <<" X2="<< x2 <<endl;
+    Swap(x1, x2);
+    cout << "X1=" << x1 << " X2=" << x2 << endl;
+
+   
+    return 0;
+}
+
